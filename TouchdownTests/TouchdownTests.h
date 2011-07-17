e8533cf33647d6a14e3537eb48d0a11e48edd93c
Jeff Remer <jeff@threestarchina.com>
2011-07-16 19:25:41 -0700

    First commit

---
+++ TouchdownTests/TouchdownTests.h
@@ -0,0 +1,17 @@
+//
+//  TouchdownTests.h
+//  TouchdownTests
+//
+//  Created by Jeff Remer on 7/16/11.
+//  Copyright 2011 Widgetbox, Inc. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface TouchdownTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ TouchdownTests/TouchdownTests.m
@@ -0,0 +1,33 @@
+//
+//  TouchdownTests.m
+//  TouchdownTests
+//
+//  Created by Jeff Remer on 7/16/11.
+//  Copyright 2011 Widgetbox, Inc. All rights reserved.
+//
+
+#import "TouchdownTests.h"
+
+
+@implementation TouchdownTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TouchdownTests");
+}
+
+@end
